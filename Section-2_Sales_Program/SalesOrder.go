e9c71bca671c3c0f01fbb6cb3f9fcd97ce8f8ef5
Rohit-Sarkar55 <sarkarrohit650@gmail.com>
2026-09-07 22:15:00 +0530

    Second Program : understood maps syntax, functions, comma ok idiom ( multi value assignments)

---
+++ Section-2_Sales_Program/SalesOrder.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var productPrices = map[string]float64{
+	"TSHIRT": 20.00,
+	"MUG":    12.50,
+	"HAT":    18.00,
+	"BOOK":   25.99,
+}
+
+func calculateItemPrice(itemCode string) (float64, bool) {
+	basePrice, found := productPrices[itemCode]
+	if !found {
+		if strings.HasSuffix(itemCode, "_SALE") {
+			originalItemCode := strings.TrimSuffix(itemCode, "_SALE")
+			basePrice, found = productPrices[originalItemCode]
+			if found {
+				salePrice := basePrice * 0.90
+				fmt.Printf(" - Item %s (Sale! Original: $%.2f, Sale Price: $%.2f)\n",
+					originalItemCode, basePrice, salePrice)
+				return salePrice, true
+			}
+		}
+
+		fmt.Printf(" - Item: %s (Product not found)\n", itemCode)
+		return 0.0, false
+	}
+
+	return basePrice, true
+
+}
+
+func main() {
+	fmt.Println("-------------- Simple Sales Order Processor------------")
+
+	orderItems := []string{
+		"TSHIRT", "MUG_SALE", "HAT", "BOOK",
+	}
+
+	var subtotal float64
+	fmt.Println("-------Processing Order Items:")
+	for ind, orderItem := range orderItems {
+		fmt.Printf("%d. %s\n", ind, orderItem)
+		price, found := calculateItemPrice(orderItem)
+
+		if found {
+			subtotal += price
+		}
+	}
+
+	fmt.Printf("Subtotal: %.2f\n", subtotal)
+
+}
