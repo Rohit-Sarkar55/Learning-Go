72c5f10f92fa41629511dadcb907bd445fb59371
Rohit-Sarkar55 <sarkarrohit650@gmail.com>
2026-09-12 23:51:43 +0530

    8th: known about diff methods and defer

---
+++ section-4/calc.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type MathError struct {
+	Operation      string
+	InputA, InputB int
+	Message        string
+}
+
+const division = "division"
+
+func (e MathError) Error() string {
+	var inputs []string
+	if e.Operation == division {
+		inputs = append(inputs, fmt.Sprintf("a: %d ", e.InputA))
+		inputs = append(inputs, fmt.Sprintf("b: %d ", e.InputB))
+	}
+
+	return fmt.Sprintf("Math error in %s (%s): %s", e.Operation,
+		strings.Join(inputs, ","), e.Message)
+}
+
+func add(number ...int) int {
+	defer fmt.Println("Sum finished")
+
+	total := 0
+	for _, i := range number {
+		total += i
+	}
+	return total
+}
+
+func safeDivision(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, &MathError{Operation: "division", InputA: a,
+			InputB: b, Message: "Division by Zero"}
+	}
+	return a / b, nil
+}
+
+func main() {
+	fmt.Println(add(1, 2, 3))
+	ans, err := safeDivision(10, 0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(ans)
+	}
+	fmt.Println(safeDivision(40, 12))
+}
