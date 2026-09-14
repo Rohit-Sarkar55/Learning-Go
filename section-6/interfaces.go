4c443cb4224e669a05fa464b9a930600b6640d78
Rohit-Sarkar55 <sarkarrohit650@gmail.com>
2026-09-14 14:36:16 +0530

    learnt about stringer interface

--- section-6/interfaces.go
+++ section-6/interfaces.go
@@ -5,6 +5,10 @@ import "fmt"
 type Person interface {
 	GetName() string
 }
+
+type FL2 float32
+type FL3 float32
+
 type Employee struct {
 	ID   int
 	Name string
@@ -23,11 +27,39 @@ func (e Employee) GetName() string {
 func displayPerson(p Person) {
 	fmt.Println(p.GetName())
 }
+
+// stringer interface
+
+func (b BusinessPerson) String() string {
+	return fmt.Sprintf("Business Person [ID: %d , Name: %s ]", b.ID, b.Name)
+}
+func (b Employee) String() string {
+	return fmt.Sprintf("Employee [ID: %d , Name: %s ]", b.ID, b.Name)
+}
+
+func (f FL2) String() string {
+	return fmt.Sprintf("This is a custome float of 2 decimals %.2f", f)
+}
+
+func (f FL3) String() string {
+	return fmt.Sprintf("This is a custome float of 3 decimals %.3f", f)
+}
 func main() {
 
 	e1 := Employee{1, "John"}
 	b1 := BusinessPerson{1, "Alice"}
 	displayPerson(e1)
 	displayPerson(b1)
+
+	fmt.Println(e1)
+	fmt.Println(b1)
+
+	//stringer works on primitive data type is well
+
+	f1 := FL2(3.1341531)
+	f2 := FL3(3.1341531)
+
+	fmt.Println(f1)
+	fmt.Println(f2)
 
 }
